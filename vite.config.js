cebdb233e61880505ffe08f013fff7cd12b14b27
Fernando Martínez <carlos.mtzvelez@gmail.com>
2026-01-07 21:20:07 -0600

    Corrigiendo base path en vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BalletFitByJen/'
+  // AQUÍ ESTÁ LA MAGIA: El nombre exacto de tu repositorio entre barras
+  base: '/BalletFitByJen/',
 })
